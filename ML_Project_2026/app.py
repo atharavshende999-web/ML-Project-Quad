cc561881dd9f6a5186ad7c79de28e4e1b45d5ff0
atharavshende999-web <atharavshende999@gmail.com>
2026-08-01 18:08:04 +0530

    Update app.py

--- ML_Project_2026/app.py
+++ ML_Project_2026/app.py
@@ -141,10 +141,7 @@ elif menu == "🔐 Admin Panel":
 
                 st.success("✅ Access Granted")
 
-                # Show files
-                st.subheader("📂 Server Files")
-                st.write(os.listdir())
-
+                
                 # Show Excel data
                 try:
                     df = pd.read_excel("user_inputs.xlsx")
